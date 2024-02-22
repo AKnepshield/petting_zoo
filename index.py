fca86f27a6af08d2b6b4d6a48f5db31cc28877ad
Charles Knepshield <aknepshield@gmail.com>
2024-02-22 14:25:36 -0600

    Feeding shifts

--- index.py
+++ index.py
@@ -5,36 +5,63 @@ from slithering import CopperheadSnake, RatSnake, Turtle, Gecko, Scorpion
 from swimming import MallardDuck, GoldFish, SnappingTurtle
 from flying import Parakeet
 
-llama = Llama("Llama Del Ray")
-print(llama)
-donkey = Donkey("Wonkey the donkey")
-print(donkey)
-goat = Goat("Baaba Fett")
-print(goat)
+llama_del_ray = Llama("Llama Del Ray", "Midday")
+print(llama_del_ray)
+print(
+    f"{llama_del_ray.name} the {llama_del_ray.species} is available to pet during the {llama_del_ray.shift}\n"
+)
+wonkey = Donkey("Wonkey the donkey", "Afternoon")
+print(wonkey)
+print(
+    f"{wonkey.name} the {wonkey.species} is available to pet during the {wonkey.shift}\n"
+)
+baaba_fett = Goat("Baaba Fett", "Midday")
+print(baaba_fett)
+print(
+    f"{baaba_fett.name} the {baaba_fett.species} is available to pet during the {baaba_fett.shift}\n"
+)
 copperhead_snake = CopperheadSnake("Krom")
 print(copperhead_snake)
+print()
 rat_snake = RatSnake("Serpentor")
 print(rat_snake)
+print()
 mallard_duck = MallardDuck("Dewey")
 print(mallard_duck)
+print()
 gold_fish = GoldFish("Wanda")
 print(gold_fish)
-horse = Horse("Hidalgo")
-print(horse)
+print()
+hidalgo = Horse("Hidalgo", "Morning")
+print(hidalgo)
+print(
+    f"{hidalgo.name} the {hidalgo.species} is available to pet during the {hidalgo.shift}\n"
+)
 turtle = Turtle("Leo")
 print(turtle)
-rat = Rat("Splinter")
-print(rat)
+print()
+splinter = Rat("Splinter", "Afternoon")
+print(splinter)
+print(
+    f"{splinter.name} the {splinter.species} is available to pet during the {splinter.shift}\n"
+)
 gecko = Gecko("Spike")
 print(gecko)
+print()
 parakeet = Parakeet("Toxie")
 print(parakeet)
+print()
 snapping_turtle = SnappingTurtle("Slash")
 print(snapping_turtle)
+print()
 scorpion = Scorpion("Scorpius")
 print(scorpion)
-bull = Bull("Dwayne")
-print(bull)
+print()
+dwayne = Bull("Dwayne", "Morning")
+print(dwayne)
+print(
+    f"{dwayne.name} the {dwayne.species} is available to pet during the {dwayne.shift}\n"
+)
 
 
 # class Complex:
